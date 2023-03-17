91734259d17570a028129a1cb4ec5a1b9cd636ed
JThungu <josephkangethe848@gmail.com>
2023-03-16 20:56:56 -0700

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,8 +5,9 @@
 * explain what your code does
 * Return: 1
 */
+
 int main(void)
 {
-       write(2, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
-       return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
