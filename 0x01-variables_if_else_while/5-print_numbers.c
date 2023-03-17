842f2c508a0bc79d4ce35174facf2ae7039cc381
JThungu <josephkangethe848@gmail.com>
2023-03-17 16:41:13 -0700

    Digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Single Digits'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int k = 97;
+		
+	for (k = 0; k <= 9; k++)
+	{
+		printf("%d", k);
+	}
+	printf("\n");
+	return (0);
+}
