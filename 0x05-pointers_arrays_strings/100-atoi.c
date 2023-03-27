4484416ff415a7861019d026db4f751398bcdd3d
JThungu <josephkangethe848@gmail.com>
2023-03-27 14:49:51 -0700

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,7 +26,7 @@ int _atoi(char *s)
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-				digit = -digit;	
+				digit = -digit;
 			m = m * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
