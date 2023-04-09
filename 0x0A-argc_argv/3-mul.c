6ebfe1c154cb3e02b641ca2748d4b43a05c0f0fa
JThungu <josephkangethe848@gmail.com>
2023-04-09 15:27:23 -0700

    first commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point for the program
+ *
+ * This function takes two command-line arguments, converts them to integers,
+ * multiplies them together, and prints the result to stdout.
+ *
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings representing the command-line arguments
+ *
+ * Return: 0 if successful, 1 if an error occurred
  */
+
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
@@ -14,7 +20,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-	return (1);
+		return (1);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
