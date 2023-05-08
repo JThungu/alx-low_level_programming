1619182b6c3f1b860ebc70b405d8f557356c729d
JThungu <josephkangethe848@gmail.com>
2023-05-08 15:11:52 -0700

    output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints its content to STDOUT.
+ * @filename: The name of the file to be read.
+ * @letters: The number of bytes to read and print.
+ *
+ * Return: The actual number of bytes read and printed, or 0 if the function
+ * fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, r, w;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1 || w != r)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (w);
+}
