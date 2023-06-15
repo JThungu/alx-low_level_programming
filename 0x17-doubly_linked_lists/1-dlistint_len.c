196968e9670f34994dbaae849818ef6713558df3
JThungu <josephkangethe848@gmail.com>
2023-06-15 12:09:44 -0700

    three
    u

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - number of elements in a linked dlistint_t list.
+ * @h: A pointer to the head of the doubly linked list
+ * Return: The number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
